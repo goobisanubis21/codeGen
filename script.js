787cc7fdc4556f82975322fac73df96e9b3ef9a6
Anthony Parrino <anthony21parrino21@gmail.com>
2020-09-24 22:01:45 -0400

    started js files coding

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+//create var to hold our characters
+
+var specialCharacters = ["~", "`", ];
+
+var numericalValues = ["1234567890".split()];
+
+var lowerCaseLetters = ["abcdefghijklmnopqrstuvwxyz".split()];
+
+//create a function to prompt user for password options
+
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt("How many character would you like your password to contain?")
+  );
+}
+
+if (isNaN(length) === true) {
+  alert("Password length must be provided with a number");
+  return;
+}
+
+//conditional statement to check if password length is true
+
+if ((length < 8) || (length > 128)) {
+  alert("Password character not in range please choose a number between 8 and 128")
+  return;
+}
+
+//asking the user what characters they want (special numbers, lower or upper)
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
